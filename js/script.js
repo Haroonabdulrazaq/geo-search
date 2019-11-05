c5797707a77b85427e9df77164b9fb63daf523e3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2019-11-05 11:50:03 -0700

    Display local weather condition

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+//API KEY : AIzaSyDnkEl_UehdS23iDfq3rjdWyvvSuAnKhv8
 //Getting Element from the DOM
 let searchButton = document.querySelector('#searchButton');
 let form = document.querySelector('#form');
 let input = document.querySelector('#input');
 let loader = document.querySelector('.loader');
+let popUp = document.querySelector('.pop-up');
 let secondSection= document.querySelector('.second-section');
 
 //Add Event Listener
@@ -13,10 +15,11 @@ searchButton.addEventListener('click',($event)=>{
     const inputValue = input.value;
     if( inputValue.match(pattern)){
         error.innerHTML ='';
-        alert("We are correct");
+       popUp.setAttribute('style', 'display:block');
     }else{    
         error.innerHTML = '<br>Invalid input';
         error.classList.add('errorMessage');
+        popUp.setAttribute('style', 'display:none');
 
         //After an Invalid Input
         form.reset();
